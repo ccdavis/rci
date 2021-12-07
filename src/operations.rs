0bc4adacab69575acb71ef3a0945800732466ded
Colin Davis <ccd.umn.edu>
2021-12-06 21:42:56 -0600

    Implemented basic operations

--- src/operations.rs
+++ src/operations.rs
@@ -7,6 +7,7 @@ use crate::expression::ReturnValue;
 
 
 use TokenType::*;
+
 pub fn add(left: &TokenType, right: &TokenType) -> Result<ReturnValue, String> {
     match (left,right) {
         (Number(l), Number(r)) => 
@@ -23,38 +24,124 @@ pub fn add(left: &TokenType, right: &TokenType) -> Result<ReturnValue, String> {
     }
 }
 
-pub fn subtract(left: &TokenType, right: &TokenType) -> Result<TokenType, String> {
-    Err("not implemented".to_string())
+pub fn subtract(left: &TokenType, right: &TokenType) -> Result<ReturnValue, String> {
+    match (left,right) {
+        (Number(l), Number(r)) => 
+			Ok(ReturnValue::Value(Number(l - r))),     
+        _ => Err(format!(
+            "Can't apply '+' to types '{}' and '{}'",
+            left.print(),
+            right.print()
+        )),
+    }    
 }
 
-pub fn multiply(left: &TokenType, right: &TokenType) -> Result<TokenType, String> {
-	Err("not implemented".to_string())
+pub fn multiply(left: &TokenType, right: &TokenType) -> Result<ReturnValue, String> {	
+	match (left,right) {
+		(Number(l), Number(r)) => 
+			Ok(ReturnValue::Value(Number(l * r))),     
+        _ => Err(format!(
+            "Can't apply '+' to types '{}' and '{}'",
+            left.print(),
+            right.print()
+        )),
+    }    
 }
 
-pub fn divide(left: &TokenType, right: &TokenType) -> Result<TokenType, String> {
-	Err("not implemented".to_string())    
+pub fn divide(left: &TokenType, right: &TokenType) -> Result<ReturnValue, String> {	
+	match (left,right) {
+		(Number(l), Number(r)) => 
+			Ok(ReturnValue::Value(Number(l / r))),     
+        _ => Err(format!(
+            "Can't apply '+' to types '{}' and '{}'",
+            left.print(),
+            right.print()
+        )),
+    }    
 }
 
-pub fn compare_gt(left: &TokenType, right: &TokenType) -> Result<TokenType, String> {
-	Err("not implemented".to_string())        
+fn make_bool(val:bool) -> Result<ReturnValue,String> {
+	Ok(if val {
+		ReturnValue::Value(True)
+	} else {
+		ReturnValue::Value(False)
+	})
 }
 
-pub fn compare_lt(left: &TokenType, right: &TokenType) -> Result<TokenType, String> {
-	Err("not implemented".to_string())            
+pub fn compare_gt(left: &TokenType, right: &TokenType) -> Result<ReturnValue, String> {	
+	match (left,right) {
+		(Number(l), Number(r)) => make_bool(l > r),			
+        _ => Err(format!(
+            "Can't apply '+' to types '{}' and '{}'",
+            left.print(),
+            right.print()
+        )),
+    }    
 }
 
-pub fn compare_lte(left: &TokenType, right: &TokenType) -> Result<TokenType, String> {
-	Err("not implemented".to_string())            
+pub fn compare_lt(left: &TokenType, right: &TokenType) -> Result<ReturnValue, String> {
+	match (left,right) {
+		(Number(l), Number(r)) => make_bool(l < r),			
+        _ => Err(format!(
+            "Can't apply '+' to types '{}' and '{}'",
+            left.print(),
+            right.print()
+        )),
+    }    
+	
 }
 
-pub fn compare_gte(left: &TokenType, right: &TokenType) -> Result<TokenType, String> {
-	Err("not implemented".to_string())                
+pub fn compare_lte(left: &TokenType, right: &TokenType) -> Result<ReturnValue, String> {	
+	match (left,right) {
+		(Number(l), Number(r)) => make_bool(l <= r),			
+        _ => Err(format!(
+            "Can't apply '+' to types '{}' and '{}'",
+            left.print(),
+            right.print()
+        )),
+    }    	
 }
 
-pub fn not_equal(left: &TokenType, right: &TokenType) -> Result<TokenType, String> {
-	Err("not implemented".to_string())                
+pub fn compare_gte(left: &TokenType, right: &TokenType) -> Result<ReturnValue, String> {	
+	match (left,right) {
+		(Number(l), Number(r)) => make_bool(l >= r),			
+        _ => Err(format!(
+            "Can't apply '+' to types '{}' and '{}'",
+            left.print(),
+            right.print()
+        )),
+    }    	
 }
 
-pub fn equal(left: &TokenType, right: &TokenType) -> Result<TokenType, String> {
-	Err("not implemented".to_string())                
+pub fn not_equal(left: &TokenType, right: &TokenType) -> Result<ReturnValue, String> {	
+	match (left,right) {
+		(Number(l), Number(r)) => make_bool(l != r),			
+		(Str(l), Str(r)) => make_bool(l != r),
+		(True, True) => Ok(ReturnValue::Value(False)),
+		(False, False) => Ok(ReturnValue::Value(False)),
+		(True, False) => Ok(ReturnValue::Value(True)),
+		(False, True) => Ok(ReturnValue::Value(True)),
+        _ => Err(format!(
+            "Can't apply '+' to types '{}' and '{}'",
+            left.print(),
+            right.print()
+        )),
+    }    	
+}
+
+pub fn equal(left: &TokenType, right: &TokenType) -> Result<ReturnValue, String> {
+	match (left,right) {
+		(Number(l), Number(r)) => make_bool(l == r),			
+		(Str(l), Str(r)) => make_bool(l == r),			
+		(True, True) => Ok(ReturnValue::Value(True)),
+		(False, False) => Ok(ReturnValue::Value(True)),
+		(True, False) => Ok(ReturnValue::Value(False)),
+		(False, True) => Ok(ReturnValue::Value(False)),
+        _ => Err(format!(
+            "Can't apply '+' to types '{}' and '{}'",
+            left.print(),
+            right.print()
+        )),
+    }    	
+	
 }
