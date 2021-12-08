49dd24c7d89717f816727164b6c7bb2541d9dae4
Colin Davis <ccd.umn.edu>
2021-12-07 20:33:57 -0600

    Statement constructor helpers

--- src/statement.rs
+++ src/statement.rs
@@ -6,8 +6,8 @@ enum Stmt {
 	Block(BlockNode),		
 	If(IfNode),
 	While(WhileNode),
-	
 }
+
 #[derive(Clone,Debug)]
 struct PrintNode {
 	expression : Expr,
@@ -34,4 +34,40 @@ struct WhileNode {
 	condition : Expr,
 	body : Box<Stmt>,
 }
+
+impl Stmt {
+	
+
+	pub fn print_stmt(expression : Expr) -> Stmt {
+		Stmt::Print(PrintNode{ expression })
+	}
+	
+	pub fn expression_stmt(expression : Expr) -> Stmt {
+		Stmt::ExpressionStmt( ExpressionStmtNode { expression })
+	}
+	
+	pub fn block_stmt(statements:Vec<Stmt>) -> Stmt {
+		Stmt::Block(BlockNode { statements })
+	}
+	
+	pub fn if_stmt(
+		condition: Expr, 
+		then_branch: Stmt, 
+		else_branch: Stmt) -> Stmt {
+		
+		Stmt::If(IfNode { 
+			condition, 
+			then_branch: Box::new(then_branch), 
+			else_branch: Box::new(else_branch)
+		})
+	}
+	
+	pub fn while_stmt(condition: Expr, body: Stmt) -> Stmt {
+		Stmt::While(WhileNode { condition, body : Box::new(body) } )
+	}
+	
+	
+	
+}
+
 
