8e1c96fa17b151cc83b6384114075b6588b80048
Colin Davis <ccd.umn.edu>
2022-02-13 15:19:10 -0600

    Use better error types

---
+++ src/errors.rs
@@ -0,0 +1,68 @@
+use crate::lex::Token;
+use std::fmt;
+#[derive(Clone, Debug, PartialEq)]
+pub enum ErrorType {
+    Type,
+    Parse,
+    Execution,
+    Evaluation,
+    Internal,
+}
+
+impl fmt::Display for ErrorType {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        use ErrorType::*;
+        let name = match self {
+            Type => "Type",
+            Parse => "Parse",
+            Evaluation => "Evaluation",
+            Execution => "Execution",
+            Internal => "Internal",
+        };
+        write!(f, "{} Error", &name)
+    }
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub struct Error {
+    pub error_type: ErrorType,
+    pub ln: usize,
+    pub col: usize,
+    pub message: String,
+}
+
+impl Error {
+    pub fn new(t: &Token, error: ErrorType, msg: String) -> Self {
+        Self {
+            ln: t.line,
+            col: t.column,
+            error_type: error,
+            message: msg,
+        }
+    }
+
+    pub fn no_location(error: ErrorType, msg: String) -> Self {
+        Self {
+            ln: 0,
+            col: 0,
+            error_type: error,
+            message: msg,
+        }
+    }
+
+    pub fn internal(error: ErrorType, msg: String) -> Self {
+        Error::no_location(error, msg)
+    }
+
+    pub fn format(&self) -> String {
+        format!(
+            "{} at {}, {}: {}",
+            self.error_type, self.ln, self.col, self.message
+        )
+    }
+
+    // Use the ln and col to add a line of source and a ^ to show the error location.
+    pub fn show_with_source(&self, source: String) -> String {
+        self.format()
+    }
+}
