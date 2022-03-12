72d590b2af1d751363279b3497b7e0f7a3506ec5
Colin Davis <ccd.umn.edu>
2022-03-12 17:31:10 -0600

    Adjust usage for tcc compatibility

--- ir/compiler_support_test.c
+++ ir/compiler_support_test.c
@@ -2,17 +2,21 @@
 
 int main() {
 	
-	rci_data x = (rci_data) 25.0;
-	rci_data y = (rci_data) (rci_number)8;
+	
+	rci_data x;
+	x._number = 8.0;
+	rci_data y;
+	y._number = (double) 25;
+	
 	
 	rci_data result;
 	result._number =  x._number + y._number;
 //	binary_operation(_ADD_, x,y);
 	
 	printf("%f", result._number);
-		
+
 	rci_data str_result;	
-	str_result._string = (rci_str) {.len=3, .data = "abc", .encoding = byte_encoded }; 
+	str_result._string = (rci_str) {.data = "abc",.len=3,  .encoding = byte_encoded }; 
 	
 	rci_str  this_string = str_result._string;
 	printf("\n%ld, '%s'", this_string.len, this_string.data);
