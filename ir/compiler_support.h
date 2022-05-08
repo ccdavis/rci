d0c609fdb9effd295c0ebeb06e29cd3460349767
Colin Davis <ccd.umn.edu>
2022-05-08 15:18:41 -0500

    IR in progress, next step to builadd allocate and deallocate object types and track for garbage collection.

--- ir/compiler_support.h
+++ ir/compiler_support.h
@@ -5,10 +5,10 @@
 #include <string.h>
 #include <stdlib.h>
 
+// Alias primitive data types
 typedef unsigned char rci_bool;
 #define true 1
 #define false 0
-
 typedef long rci_int;
 typedef double rci_number;
 
@@ -16,31 +16,71 @@ typedef enum { utf_8_encoded, byte_encoded}
 	char_encoding;
 	
 
+// All the types that can have literals
 typedef enum { _number_, _string_ , _boolean_, _array_ } 
 	rci_type;	
 	
 
 typedef enum { _NEGATIVE_, _NOT_} rci_unary_operators;
-// These all result in number
+
 typedef enum { _ADD_, _SUB_, _MUL_, _DIV_, _MOD_, _POW_, _SHL_, _SHR_,  
 			_LT_, _GT_, _LTE_, _GTE_, _EQ_, _NE_, _AND_, _OR_ }  
 	rci_binary_operators;
 
-typedef struct {	
-	char * data;
+
+typedef enum {
+	object_array,
+	object_string,
+	object_map,
+	object_set,
+	object_record
+} rci_object_type;
+
+struct rci_object {
+	rci_object_type type;
+	// memory management info goes here
+};
+
+
+typedef union {
+	rci_number _number;
+	rci_bool _boolean;	
+	rci_object * _object;
+} rci_data;
+
+// For run-time type information
+typedef struct {
+	rci_data data;	
+	rci_type type;	
+}  rci_value;
+
+/* ************************************************************************************************
+
+
+Implementation of complex types
+
+*/
+
+struct rci_str {		
 	long len;	 // number of bytes
 	long chars; // numbers of chars
-	long refs;
+	long refs; // basically is it a literal or a heap value
 	char_encoding encoding;
-} rci_str;
+	char * data;
+}; 
+
+struct StringObject {
+	rci_object obj;
+	rci_str string_data;
+};
 
 rci_str rci_str_ascii_literal(char  str[]) {
-	rci_str result = {
-		.data = str,
+	rci_str result = {		
 		.len = strlen(str),
 		.chars = strlen(str),
 		.refs = 0,
-		.encoding = byte_encoded
+		.encoding = byte_encoded,
+		.data = str
 	};
 	return result;
 }
@@ -57,12 +97,12 @@ rci_str new_rci_str(char  data[], char_encoding enc) {
 	int byte_length = strlen(data);	
 	char * memory = malloc(byte_length + 1);
 	strcpy(memory, data);
-	return (rci_str) {
-		.data = memory,
+	return (rci_str) {		
 		.len = byte_length,
 		.chars = byte_length,
 		.refs = 1,
-		.encoding = enc				
+		.encoding = enc,				
+		.data = memory
 	};
 }
 
@@ -72,66 +112,78 @@ rci_str cat_rci_str(rci_str left, rci_str right) {
 	memcpy(new_data, left.data, left.len);
 	memcpy(new_data + left.len, right.data, right.len);
 	new_data[new_len] = '\0';
-	rci_str new_string= {
-		.data = new_data, 
+	rci_str new_string= {		
 		.len = new_len, 
 		.chars = left.chars + right.chars,
-		.refs = 1 ,
-		.encoding = left.encoding 		
+		.refs = 1,
+		.encoding = left.encoding,
+		.data = new_data 
 	};	
 	return new_string;
 }	
 
-rci_str * pass_rci_str(rci_str  *value) {
-	value->refs += 1;
-	return value;
+struct rci_array {	
+	long len;
+	rci_type type;	
+	rci_data * elements;	
+};
+
+struct ArrayObject {
+	rci_obj obj;
+	rci_array array_data;
+};
+
+
+rci_value new_array(rci_type element_type,rci_value * initial_data, long initial_len) {
+	ArrayObject new_array = malloc(sizeof(ArrayObject));
+	new_array->obj.type = object_array;
+	new_array->array_data.len = initial_len;
+	new_array->array_data.type = element_type;
+	new_array->array_data.elements = malloc(initial_len * sizeof(rci_data));
+	for (int e=0; e<initial_len; e++) {
+		new_array->array_data.elements[e] = initial_data[e].data;		
+	}
+	return (rci_value) {.data._object = new_array,.type=_array_};
 }
 
-void exit_scope_rci_str(rci_str *value) {
-	// If refs == 0 it's a literal and 
-	// no memory needs to be managed.
-	if (value->refs == 1) {
-		free(value->data);
-		value->refs = -1;
-	} else  if (value->refs > 1) {
-		value->refs -= 1;
+// Add a little run-time checking
+rci_value array_lookup(rci_array this_array, long index) {
+	if (index>= this_array.len) {
+		runtime_error("Index out of bounds.");
 	}
+	return (rci_value) {.data = this_array[index], .type = this_array.type};	
 }
 
-rci_str * return_rci_str(rci_str *value) {
-	// If refs == 0 or 1 this is a 'move', the
-	// change in scope decrements the count normally
-	// but the memory won't be needed in the scope
-	// we're exiting.
-	if (value->refs == 0 || value->refs == 1) return value;
-	if (value->refs > 1) {
-		value->refs -= 1;
-		return value;
+void replace_element(rci_array this_array, long index, rci_value new_element) {	
+	if (new_element.type != this_array.type) {
+		code_gen_error("Wrong element type for array element type.");
 	}
-	if (value->refs == -1) {
-		printf("Runtime error: String memory deallocated.");
-		exit(1);
+	
+	if (index >= this_array.len) {
+		runtime_error("Index out of bounds.");
 	}
+	
+	this_array[index] = new_element.data;	
 }
 
 
+
+/* ***************************************************************************
+
+	Debuging support
+*/
 
 void debug_str_to_stdout(rci_str s) {
 	printf("string data: '%s', len: %d, chars: %d",s.data,s.len,s.chars);
 }
 
 
-typedef union {
-	rci_number _number;
-	rci_bool _boolean;
-	rci_str _string;			
-} rci_data;
 
-// For run-time type information
-typedef struct {
-	rci_data data;	
-	rci_type type;	
-}  rci_value;
+int is_type(rci_value v, rci_type t) {
+	return v.type == t;
+}
+
+
 
 void debug_value_to_stdout(rci_value value) {
 	switch (value.type) {
@@ -158,31 +210,39 @@ void debug_value_to_stdout(rci_value value) {
 	}
 }
 
-typedef struct {
-	rci_data * elements;	
-	long len;
-	rci_type type;	
-} rci_array;
-
 
 void code_gen_error(const char * msg) {
-	printf("%s",msg);
+	printf("Code generation error: %s",msg);
+	exit(1);
+}
+
+void runtime_error(const char * msg) {
+	printf("Runtime error: %s",msg);
 	exit(1);
 }
 
 
+
+/* *********************************************************
+ Compiler support and stdlib
+
+*/
+
 rci_value cat_string(rci_value lhs, rci_value rhs) {
-	return (rci_value) {.data._string = (rci_str) cat_rci_str(lhs.data._string,rhs.data._string), .type=_string_};
+	StringObject new_string = malloc(sizeof(StringObject));
+	new_string->obj.type = object_string;
+	new_string->string_data = cat_rci_str(lhs.data._object->string_data,rhs.data._object->string_data);
+	
+	return (rci_value) {.data._object = new_string, .type = _string_};
 }
 
 rci_value assign_string(rci_value lhs, rci_value rhs) {
-	if (lhs.data._string.refs > 0) {
-		free(lhs.data._string.data);
+	if (lhs.data._object->refs > 0) {
+		free(lhs.data._object->string_data.data);
 	}
-	return rhs;
+	lhs.data._object.string_data = rhs.data._object.string_data;
+	return lhs;
 }
-
-
 
 
 rci_value power(rci_value x,rci_value p) {
@@ -293,6 +353,11 @@ rci_value unary_operation(rci_unary_operators op, rci_value value) {
 	}
 }
 
+/*  ****************************************************************
+
+ C object code generation support
+ 
+*/
 unsigned char  rci_value_to_c_boolean(rci_value v) {	
 	return (unsigned char) v.data._boolean;
 }
