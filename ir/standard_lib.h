eb60ccf364f0929e836d57ad1168eac33cc83797
Colin Davis <ccd.umn.edu>
2023-01-07 17:59:22 -0600

    Added random, random_seed, clock_per_second, sleep to std lib

--- ir/standard_lib.h
+++ ir/standard_lib.h
@@ -5,21 +5,32 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
+#include<unistd.h>
 
 rci_value std_clock() {
-	clock_t c = clock();
+	double  c = (double)clock();	
 	return NUMBER_VAL(c);	
 }
+
+rci_value std_clock_per_second() {
+	return  NUMBER_VAL(CLOCKS_PER_SEC);
+}
+
 
 rci_value std_random() {
 	return NUMBER_VAL(rand());
 }
 
-rci_value seed(rci_value s) {
+rci_value std_random_seed(rci_value s) {
 	rci_value v = NUMBER_VAL(0);
 	srand(AS_NUMBER(s));
 	return v;
 }
+
+rci_value std_sleep(rci_value secs) {
+	return NUMBER_VAL(sleep(AS_NUMBER(secs)));
+}
+
 
 
 
